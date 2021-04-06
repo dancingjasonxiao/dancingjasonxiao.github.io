7a3441da1c2a6b71ba16bc78b24196730578db72
dancingjasonxiao <dancingjasonxiao@163.com>
2021-04-06 13:19:20 +0800

    perf: 修改自动部署脚本指向仓库地址

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:dancingjasonxiao/dancingjasonxiao.github.io.git main:gh-pages
+git push -f git@github.com:dancingjasonxiao/dancingjasonxiao.github.io.git master:gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:dancingjasonxiao/blog.git main:gh-pages
+# git push -f git@github.com:dancingjasonxiao/blog.git master:gh-pages
 
 cd -
